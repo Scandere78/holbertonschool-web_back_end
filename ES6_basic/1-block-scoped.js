9f19eb8eebbc3912d8b83418dabb0ca6b1920ebf
ScandereT78 <scanderetejpro@gmail.com>
2024-11-09 15:39:32 +0100

    fix

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,10 +3,8 @@ export default function taskBlock(trueOrFalse) {
     const task2 = true;
   
     if (trueOrFalse) {
-      task = true;
-      task2 = false;
+      return [task, task2];
     }
   
     return [task, task2];
   }
-  
