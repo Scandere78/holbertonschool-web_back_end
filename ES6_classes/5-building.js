e92b2e2fdc76360b7b996bcc381169d162933102
ScandereT78 <scanderetejpro@gmail.com>
2024-11-11 22:07:51 +0100

    start the project and add the task 0-5

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,17 @@
+export default class Building {
+    constructor(sqft) {
+      if (new.target === Building) {
+        throw new Error("Cannot instantiate an abstract class");
+      }
+      this._sqft = sqft;
+    }
+  
+    get sqft() {
+      return this._sqft;
+    }
+  
+    evacuationWarningMessage() {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+  
