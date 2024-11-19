2bde61a387355787618ab43856ac59f897b248db
ScandereT78 <scanderetejpro@gmail.com>
2024-11-19 17:55:33 +0100

    fix startsWith if startStrinh is null

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -3,7 +3,7 @@ export default function cleanSet(set, startString) {
     return '';
   }
 
-  let result = [];
+  const result = [];
   for (const value of set) {
     if (typeof value === 'string' && value.startsWith(startString)) {
       result.push(value.slice(startString.length));
