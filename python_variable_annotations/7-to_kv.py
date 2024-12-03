5cd1ad08ca15d9954bb022d2fc036a0962e0827e
ScandereT78 <scanderetejpro@gmail.com>
2024-12-03 11:52:07 +0100

    fix the project variables annotations

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 from typing import Union, Tuple
 
-# Define the function to_kv that takes a string and a number (int or float), and returns a tuple
+
+# function to_kv that takes a string and a number, and returns a tuple
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    # Return a tuple with the string and the square of the number (converted to float)
+    # Return a tuple with the string and (converted to float)
     return (k, float(v ** 2))
--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 from typing import Callable
 
-# Define the function make_multiplier that returns a function to multiply a number by multiplier
+
+# make_multiplier that returns a function to multiply a number by multiplier
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    # Define and return a function that multiplies its input by the multiplier
+    # return a function that multiplies its input by the multiplier
     return lambda x: x * multiplier
