87560d20f7cd9d89d8a4c7e0278c386c2772039f
ScandereT78 <scanderetejpro@gmail.com>
2024-12-10 16:27:10 +0100

    fix the bug in task 01

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
-'''Async Comprehensions'''
+"""Async comprehension"""
+import asyncio
 from typing import List
-Vector = List[float]
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Vector:
-    ''' Collect 10 random numbers using async_generator '''
-    return [y async for y in async_generator()]
+async def async_comprehension() -> List[float]:
+    """Collect 10 random numbers using an async comprehensing"""
+    return [i async for i in async_generator()]
+
+if __name__ == "__main__":
+    asyncio.run(async_comprehension())
