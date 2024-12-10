b89b79fcef854209cea1a9097a17c973f1553a8c
ScandereT78 <scanderetejpro@gmail.com>
2024-12-10 16:24:02 +0100

    fix pycode style

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-
-
+'''Async Comprehensions'''
 from typing import List
-
+Vector = List[float]
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List[float]:
-    return [value async for value in async_generator()]
+async def async_comprehension() -> Vector:
+    ''' Collect 10 random numbers using async_generator '''
+    return [y async for y in async_generator()]
