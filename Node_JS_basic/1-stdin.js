0ecc9e3357a5a63bd7b94a37a8f8e850b6dc2d3d
Scandere78 <scanderetejpro@gmail.com>
2025-01-21 13:01:20 +0100

    test

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,9 +1,20 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
+// Read the user's input
+rl.on('line', (input) => {
+  console.log(`Your name is: ${input}`);
+  rl.close();
+});
 
+// Print the closing message when the program ends
+rl.on('close', () => {
   console.log('This important software is now closing');
-  process.exit();
+  process.exit(0);
 });
