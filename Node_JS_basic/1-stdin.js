e8eee56ec6e14ca8b7134f21a94daaed117de491
Scandere78 <scanderetejpro@gmail.com>
2025-01-21 12:58:07 +0100

    test

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,9 +1,9 @@
-console.log("Welcome to Holberton School, what is your name?")
+console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
-    const name = data.toString().trim();
-    console.log(`Your name is: ${name}`);
+  const name = data.toString().trim();
+  console.log(`Your name is: ${name}`);
 
-    console.log("This important software is now closing")
-    process.exit();
-})
+  console.log('This important software is now closing');
+  process.exit();
+});
