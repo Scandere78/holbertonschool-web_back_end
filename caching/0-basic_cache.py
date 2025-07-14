5791281b1751c51c35b2cb9ee074885952156601
ScandereT78 <scanderetejpro@gmail.com>
2025-07-14 05:58:20 +0100

    fix pep guardiola

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -15,6 +15,7 @@ class BasicCache(BaseCaching):
         """
         if key is not None and item is not None:
             self.cache_data[key] = item
+
     def get(self, key):
         """
         Get an item by key
