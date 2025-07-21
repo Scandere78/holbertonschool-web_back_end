9674dd86d3b6083496c78b86f9e60e4012ceee94
ScandereT78 <scanderetejpro@gmail.com>
2025-07-21 17:07:51 +0100

    refactor: update get_db function to improve readability and connection handling

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -66,16 +66,19 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db() -> MySQLConnection:
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ Function that returns a connector to the database.
     """
-    Connects to a MySQL database using credentials from environment variables.
+    db_username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+    db_password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+    db_host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
+    db_name = os.getenv('PERSONAL_DATA_DB_NAME', 'my_db')
 
-    Returns:
-        MySQLConnection: A MySQL database connection object
-    """
-    return mysql.connector.connect(
-        user=os.getenv("PERSONAL_DATA_DB_USERNAME", "root"),
-        password=os.getenv("PERSONAL_DATA_DB_PASSWORD", ""),
-        host=os.getenv("PERSONAL_DATA_DB_HOST", "localhost"),
-        database=os.getenv("PERSONAL_DATA_DB_NAME")
+    connection = mysql.connector.connect(
+        user=db_username,
+        password=db_password,
+        host=db_host,
+        database=db_name
     )
+
+    return connection
