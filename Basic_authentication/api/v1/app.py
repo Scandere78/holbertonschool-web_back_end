00271face4c6170bee59a0f5c86b24ce988ab341
ScandereT78 <scanderetejpro@gmail.com>
2025-07-22 17:56:48 +0100

    feat: implement request authentication and error handling in app module

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -7,11 +7,39 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from api.v1.views import app_views
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+auth_type = getenv("AUTH_TYPE")
+if auth_type == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+else:
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+
+@app.before_request
+def before_request_func():
+    """ Function that filter each request."""
+    if auth is None:
+        return
+
+    exempt_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+                    '/api/v1/forbidden/']
+
+    if not auth.require_auth(request.path, exempt_paths):
+        return
+
+    if auth.authorization_header(request) is None:
+        abort(401)
+
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 @app.errorhandler(404)
@@ -22,14 +50,16 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def unauthorized(error):
-    """Custom 401 error hander"""
+def unauthorized(error) -> str:
+    """ Unauthorized handler
+    """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def forbidden(error):
-    """Custom 403 error hander"""
+def forbidden(error) -> str:
+    """ Forbidden handler
+    """
     return jsonify({"error": "Forbidden"}), 403
 
 
