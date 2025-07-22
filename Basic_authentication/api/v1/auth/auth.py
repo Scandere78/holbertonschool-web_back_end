9576713066e6f76b11a1a7318a42df793d99579b
ScandereT78 <scanderetejpro@gmail.com>
2025-07-22 17:51:05 +0100

    refactor: enhance docstrings and improve readability in Auth class methods

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -1,22 +1,45 @@
 #!/usr/bin/env python3
-""" Authentication module
 """
-
+Authentication module for the API
+"""
 from flask import request
 from typing import List, TypeVar
 
 
-class Auth:
-    """ Auth class to handle authentication """
+User = TypeVar('User')
+
+
+class Auth():
+    """ Class to manage the API authentication.
+    """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Check if authentication is required for the path"""
-        return False
+        """ Determines if authentication is required for the given path.
+        """
+        if path is None:
+            return True
+
+        if not excluded_paths:
+            return True
+
+        normal_path = path if path.endswith('/') else path + '/'
+
+        normal_excluded_paths = [p if p.endswith('/') else p + '/'
+                                 for p in excluded_paths]
+
+        if normal_path in normal_excluded_paths:
+            return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
-        """Return the value of the Authorization header"""
-        return None
+        """ Retrieves the Authorization header from the request.
+        """
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers['Authorization']
 
-    def current_user(self, request=None) -> TypeVar('User'):
-        """Return the current user (None for now)"""
+    def current_user(self, request=None) -> User:
+        """ Retrieves the current user from the request.
+        """
         return None
