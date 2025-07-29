8042daaff866c621802dc84c9be4218fdde92382
ScandereT78 <scanderetejpro@gmail.com>
2025-07-29 15:03:47 +0100

    fix: improve readability in view_one_user function by formatting conditional checks

--- Session_authentication/api/v1/views/users.py
+++ Session_authentication/api/v1/views/users.py
@@ -24,7 +24,8 @@ def view_one_user(user_id: str = None) -> str:
         abort(404)
 
     if user_id == "me":
-        if not hasattr(request, 'current_user') or request.current_user is None:
+        if (not hasattr(request, 'current_user') or
+                request.current_user is None):
             abort(404)
         return jsonify(request.current_user.to_json())
 
@@ -32,6 +33,7 @@ def view_one_user(user_id: str = None) -> str:
     if user is None:
         abort(404)
     return jsonify(user.to_json())
+
 
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
 def delete_user(user_id: str = None) -> str:
