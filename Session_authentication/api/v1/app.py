30f8fdb196cd6a0bf493b818fb5b17bf6f880e3d
ScandereT78 <scanderetejpro@gmail.com>
2025-07-29 15:12:11 +0100

    feat: implement SessionAuth class for session-based authentication

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -8,7 +8,9 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.views import app_views
-
+from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -16,10 +18,10 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth_type = getenv("AUTH_TYPE")
 if auth_type == "basic_auth":
-    from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif auth_type == "session_auth":
+    auth = SessionAuth()
 else:
-    from api.v1.auth.auth import Auth
     auth = Auth()
 
 
---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Module session_auth
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ Session authentication class - inherits from Auth """
+    pass
