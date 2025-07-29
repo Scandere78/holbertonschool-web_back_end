9bae26828ed421b2c671ef51d89cd65ca1dc67c9
ScandereT78 <scanderetejpro@gmail.com>
2025-07-29 15:17:53 +0100

    feat: implement create_session method in SessionAuth class for session ID generation

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python3
 """ Module session_auth
 """
-
+import uuid
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """ Session authentication class - inherits from Auth """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Create a Session ID for a given user_id """
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+
+        # Génère un UUID4 unique en string
+        session_id = str(uuid.uuid4())
+
+        # Enregistre dans le dictionnaire : clé=session_id, valeur=user_id
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
