a7560ee8bd925c7e4f35799a9962307bdfbf15c6
ScandereT78 <scanderetejpro@gmail.com>
2025-08-04 16:07:51 +0100

    feat: add register_user method in Auth class

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -3,6 +3,9 @@
 Auth module
 """
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -18,3 +21,25 @@ def _hash_password(password: str) -> bytes:
     # bcrypt.hashpw attend des bytes, donc on encode le mot de passe en UTF-8
     hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     return hashed
+
+
+class Auth:
+    """Auth class to interact with the authentication database."""
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Register a new user.
+        Raises ValueError if user already exists.
+        """
+        try:
+            # Vérifier si un utilisateur existe déjà
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            # Hachage du mot de passe
+            hashed_pwd = _hash_password(password)
+            # Ajouter l'utilisateur à la base
+            return self._db.add_user(email, hashed_pwd.decode("utf-8"))
