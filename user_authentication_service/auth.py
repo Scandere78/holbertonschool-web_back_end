12ebf8deb88b8987981125f8f43c08dddaea1f7a
ScandereT78 <scanderetejpro@gmail.com>
2025-08-06 13:00:24 +0100

    fix

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -94,7 +94,7 @@ class Auth:
             return self._db.find_user_by(session_id=session_id)
         except NoResultFound:
             return None
-    
+
     def destroy_session(self, user_id: int) -> None:
         """ Destroys a user's session by setting their session ID to None.
         """
