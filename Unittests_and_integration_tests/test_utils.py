efe62e1cef41d048f0e1e1aa79c0cbfe3111814f
ScandereT78 <scanderetejpro@gmail.com>
2025-08-11 20:04:53 +0100

    Refactor unit tests for access_nested_map to handle exceptions

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -10,16 +10,11 @@ from utils import access_nested_map
 
 class TestAccessNestedMap(unittest.TestCase):
     """Test case for the access_nested_map function"""
-
     @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2)
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
     ])
-    def test_access_nested_map(self, nested_map, path, expected):
-        """Test that access_nested_map returns the expected value"""
-        self.assertEqual(access_nested_map(nested_map, path), expected)
-
-
-if __name__ == "__main__":
-    unittest.main()
+    def test_access_nested_map_exception(self, nested_map, path):
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), repr(path[-1]))
