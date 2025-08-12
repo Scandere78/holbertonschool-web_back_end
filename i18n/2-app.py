bf5f72d9de8ef47420fd0a84e09adf395d3c677e
ScandereT78 <scanderetejpro@gmail.com>
2025-08-12 12:38:41 +0100

    Refactor Flask app to improve Babel integration and template localization

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -1,39 +1,43 @@
 #!/usr/bin/env python3
-""" Basic Flask app Module
-"""
+""" Basic Flask app with Babel configuration """
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 from flask_babel import Babel
-
 
 app = Flask(__name__)
 
 
 class Config:
-    """ Configuration class.
-    """
+    """ Configuration for Babel """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app.config.from_object(Config)
-
-# Création de Babel et initialisation avec notre get_locale
-babel = Babel()
+babel = Babel(app)
 
 
+@babel.localeselector
 def get_locale():
     """Select the best match language."""
+    # Si ?locale=xx est fourni et valide, on l'utilise
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    # Sinon, on détecte via l'en-tête Accept-Language
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-babel.init_app(app, locale_selector=get_locale)
+@app.before_request
+def before_request():
+    """Stocke la locale choisie dans g.locale pour le template"""
+    g.locale = get_locale()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def welcome():
-    """Endpoint returning Hello world."""
+    """Affiche la page d'accueil"""
     return render_template("2-index.html")
 
 
