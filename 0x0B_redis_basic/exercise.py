73f8b9bf947031b3e381d45982e8e199d6084d09
ScandereT78 <scanderetejpro@gmail.com>
2025-08-18 17:44:32 +0200

    Enhance Cache class: add call_history decorator to track method input/output history in Redis

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -4,13 +4,41 @@ import uuid
 from typing import Union, Optional, Callable
 from functools import wraps
 
+
 def count_calls(method: Callable) -> Callable:
+    """Decorator that counts how many times a method is called"""
+
     @wraps(method)
     def wrapper(self, *args, **kwargs):
         key = method.__qualname__
         self._redis.incr(key)
         return method(self, *args, **kwargs)
+
     return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """Decorator that stores the history of inputs and outputs in Redis"""
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        # Keys for storing inputs and outputs
+        input_key = method.__qualname__ + ":inputs"
+        output_key = method.__qualname__ + ":outputs"
+
+        # Save input arguments as string
+        self._redis.rpush(input_key, str(args))
+
+        # Execute the method and get output
+        result = method(self, *args, **kwargs)
+
+        # Save output
+        self._redis.rpush(output_key, str(result))
+
+        return result
+
+    return wrapper
+
 
 class Cache:
     def __init__(self):
@@ -18,7 +46,9 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Generate a random key and store data in Redis"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
@@ -28,6 +58,7 @@ class Cache:
         key: str,
         fn: Optional[Callable] = None
     ) -> Union[str, bytes, int, float, None]:
+        """Get data from Redis and optionally apply conversion function"""
         data = self._redis.get(key)
         if data is None:
             return None
@@ -36,7 +67,9 @@ class Cache:
         return data
 
     def get_str(self, key: str) -> Optional[str]:
+        """Convert Redis bytes to str"""
         return self.get(key, fn=lambda d: d.decode("utf-8"))
 
     def get_int(self, key: str) -> Optional[int]:
+        """Convert Redis bytes to int"""
         return self.get(key, fn=int)
