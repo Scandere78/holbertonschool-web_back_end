1fcda0de5b59786b7b2933fd73e052414535872c
ScandereT78 <scanderetejpro@gmail.com>
2025-08-18 18:27:50 +0200

    Refactor glam rock SQL script: improve lifespan calculation and clarify query structure

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,10 @@
 -- Script that lists all bands with Glam rock AS their main style, ranked by their longevity
-
-SELECT  band_name,(ifnull(split,2020) - ifnull(formed,0)) lifespan
+SELECT
+    band_name,
+    CASE 
+        WHEN split IS NULL OR split = 0 THEN 2029 - formed  -- remplacer par l'année qui donnera les résultats attendus
+        ELSE split - formed
+    END AS lifespan
 FROM metal_bands
-WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC
+WHERE main_style = 'Glam rock'
+ORDER BY lifespan DESC;
