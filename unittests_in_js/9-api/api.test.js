e2fb075d14e6daf0d5941eb13f66bc236ed1dd81
ScandereT78 <scanderetejpro@gmail.com>
2025-08-25 23:02:56 +0200

    chore: initialize project with package.json and dependencies for queuing system in JS

--- unittests_in_js/9-api/api.test.js
+++ unittests_in_js/9-api/api.test.js
@@ -1,16 +1,5 @@
 const { expect } = require('chai');
 const request = require('request');
-const app = require('./api');  // on importe ton app
-
-let server;
-
-before((done) => {
-  server = app.listen(7865, done); // on démarre avant les tests
-});
-
-after((done) => {
-  server.close(done); // on ferme après les tests
-});
 
 describe('Index page', () => {
   it('should return correct status code and result', (done) => {
@@ -23,21 +12,21 @@ describe('Index page', () => {
   });
 });
 
-describe('Cart page', () => {
-  it('returns correct status code when id is a number', (done) => {
-    request('http://localhost:7865/cart/123', (err, res, text) => {
-      expect(err).to.be.null;
-      expect(res.statusCode).to.equal(200);
-      expect(text).to.equal('Payment methods for cart 123');
-      done();
+  describe('Cart page', () => {
+    it('returns correct status code when id is a number', (done) => {
+      request('http://localhost:7865/cart/123', (err, res, text) => {
+        expect(err).to.be.null;
+        expect(res.statusCode).to.equal(200);
+        expect(text).to.include('Payment methods for cart 123');
+        done();
+      });
     });
-  });
-
-  it('returns 404 status code when id is NOT a number', (done) => {
-    request('http://localhost:7865/cart/not-a-number', (err, res, text) => {
-      expect(err).to.be.null;
-      expect(res.statusCode).to.equal(404);
-      done();
+  
+    it('returns 404 status code when id is NOT a number', (done) => {
+      request('http://localhost:7865/cart/not-a-number', (err, res, text) => {
+        expect(err).to.be.null;
+        expect(res.statusCode).to.equal(404);
+        done();
+      });
     });
-  });
 });
